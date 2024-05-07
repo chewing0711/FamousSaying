6c42baa7d6c9d7299fad4b8cc73793741379cf46
ming018 <92374901+ming018@users.noreply.github.com>
2024-05-07 11:20:04 +0900

    Initial Commit

---
+++ Practice3/Practice3App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice3App.swift
+//  Practice3
+//
+//  Created by 박민규 on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
